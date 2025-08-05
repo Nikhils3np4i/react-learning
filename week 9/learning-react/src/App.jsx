8bf3ada98da2cce3ce84b009882ead7df9679c39
Nikhils3np4i <sinhanikhil366@gmail.com>
2025-08-05 13:48:27 +0530

    completed the assignment of ProfileCard

--- week 9/learning-react/src/App.jsx
+++ week 9/learning-react/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
           <PostComponent />
           <PostComponent />
         </div>
-        <ProfileCard viewers="200" impressions="1,313"/>
+        <ProfileCard viewers="200" impressions="1,313" />
       </div>
     </>
   )
@@ -46,19 +46,23 @@ function PostComponent() {
 }
 
 function ProfileCard(props) {
-  return(
+  return (
     <>
-      <div style={{height: "400px", width: "300px", background: "#fff", display: 'flex', alignItems: "center", flexDirection:"column"}}>
-        <div style={{background: "#d4d2d2ff", height: "25%", width:"100%"}}></div>
-        <img src="https://appx-wsb-gcp.akamai.net.in/subject/2023-01-17-0.17044360120951185.jpg" style={{width: 60, height: 60, position:"relative", top: -32, borderRadius:50}} />
-        <h3 style={{marginTop:"-10px"}}>Harkirat Singh</h3>
-        <p style={{marginTop: "-10px", color:"grey"}}>Working with webRTC</p>
-        <hr style={{width: "90%"}}/>
-        <div style={{display: "flex"}}>
-          <p>Profile Viewers</p>
-          <p>{props.viewers}</p>
-          <p>Post impressions</p>
-          <p>{props.impressions}</p>
+      <div style={{ height: "400px", width: "300px", background: "#fff", display: 'flex', alignItems: "center", flexDirection: "column" }}>
+        <div style={{ background: "#d4d2d2ff", height: "25%", width: "100%" }}></div>
+        <img src="https://appx-wsb-gcp.akamai.net.in/subject/2023-01-17-0.17044360120951185.jpg" style={{ width: 60, height: 60, position: "relative", top: -32, borderRadius: 50 }} />
+        <h3 style={{ marginTop: "-10px" }}>Harkirat Singh</h3>
+        <p style={{ marginTop: "-10px", color: "grey" }}>Working with webRTC</p>
+        <hr style={{ width: "90%" }} />
+        <div style={{ display: "flex", flexDirection:"column" }}>
+          <div style={{ width:"300px", display:"flex", justifyContent:"space-between"}}>
+            <p style={{marginLeft:"20px"}}>Profile Viewers</p>
+            <p style={{color:"blue", fontWeight:"bold", marginRight:"20px"}}>{props.viewers}</p>
+          </div>
+          <div style={{width:"300px", display:"flex", justifyContent:"space-between"}}>
+            <p style={{marginLeft:"20px"}}>Post impressions</p>
+            <p style={{color:"blue", fontWeight:"bold", marginRight:"20px"}}>{props.impressions}</p>
+          </div>
         </div>
       </div>
     </>
